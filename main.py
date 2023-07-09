6fd5dd73eab26e9d5234474298827c1ee8901d67
Core447 <jmulmann@protonmail.com>
2023-07-09 15:31:17 +0200

    Reorganisation of the folder structure

--- main.py
+++ main.py
@@ -8,43 +8,37 @@ from StreamDeck.DeviceManager import DeviceManager
 from StreamDeck.ImageHelpers import PILHelper
 
 # Folder location of image assets used by this example.
-ASSETS_PATH = os.path.join(os.path.dirname(__file__), "Assets")
+ASSETS_PATH = os.path.join(os.path.dirname(__file__), "assets")
 
 
 def createDeckImage(deck, icon_filename, font_filename, captions):
     # Resize the source image asset to best-fit the dimensions of a single key,
     # leaving a margin at the bottom so that we can draw the key title
     # afterwards.
-    icon = Image.open(icon_filename)
+    icon = Image.open(os.path.join(ASSETS_PATH, "images", icon_filename))
     image = PILHelper.create_scaled_image(deck, icon, margins=[0, 0, 20, 0], background=((0,0,0)))
 
     # Load a custom TrueType font and use it to overlay the key index, draw key
     # label onto the image a few pixels from the bottom of the key.
     draw = ImageDraw.Draw(image)
-    font = ImageFont.truetype(font_filename, 14)
+    
 
+    #load json from string
     captions = str(captions).replace("'",'"')
     captions = json.loads(captions)
-    print("captions: ")
-    print(captions)
+    
     
     for caption in captions:
-        print(caption[0]["text"])
+        #Load font
+        font = ImageFont.truetype(os.path.join(ASSETS_PATH, "fonts", font_filename), 14)
+        #Draw captions on to the image
         draw.text((image.width / 2, image.height*caption[0]["text-location"]), text=caption[0]["text"], font=font, anchor="ms", fill="white")
     
-    
-
-
-
     return PILHelper.to_native_format(deck, image)
 
 def keyNameToIndex(keyName):
     keyName = keyName.split("x")
     return int(keyName[0]) + int(keyName[1])*5
-
-
-#keyNameToIndex("1x2")
-#exit()
 
 
 def loadButton(deck, keyName, imagePath, captions, font):
@@ -67,7 +61,7 @@ def loadPage(deck, name: str):
 
     for button in buttons.items():
         #print(buttons[button[0]]["default-image"])
-        loadButton(deck, button[0], buttons[button[0]]["default-image"], buttons[button[0]]["captions"], "Assets/Roboto-Regular.ttf")
+        loadButton(deck, button[0], buttons[button[0]]["default-image"], buttons[button[0]]["captions"], "Roboto-Regular.ttf")
         
 
 
@@ -92,6 +86,6 @@ if __name__ == "__main__":
         deck.set_brightness(30)
 
         with deck:
-            image = render_key_image(deck, "Assets/Exit.png", "Assets/Roboto-Regular.ttf", "Test")
+            image = render_key_image(deck, "Assets/Exit.png", "Roboto-Regular.ttf", "Test")
             #image = Image.open("")
             deck.set_key_image(0, image)
