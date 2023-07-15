3b7cff0bf77943944735769e6b3c12a18d0242e8
Core447 <jmulmann@protonmail.com>
2023-07-15 14:30:35 +0200

    Add use of GTK widgets in MediaPlugin

--- plugins/MediaPlugin.py
+++ plugins/MediaPlugin.py
@@ -5,12 +5,20 @@ from time import sleep
 from pynput.keyboard import Key, Controller
 import json
 from gi.repository import Gtk, Adw, Gdk
+import subprocess
 
 class PausePlay(ActionBase):
     ACTION_NAME = "pauseplay"
     def __init__(self, pluginBase: PluginBase):
         super().__init__()
         self.pluginBase = pluginBase
+
+        #check if playerctl is installed on the system
+        self.playerctlAvailable = False
+        if self.runShellCommand("type -p playerctl") != None:
+            self.playerctlAvailable = True
+        print(self.playerctlAvailable)
+                    
     def onKeyDown(self, controller, deck, keyIndex):
         self.pluginBase.keyboard.press(Key.media_play_pause)
         sleep(0.1)
@@ -25,9 +33,20 @@ class PausePlay(ActionBase):
         return {'captions': [[{'text': 'Pause', 'font-size': 12, 'text-location': 0.5}]], 'default-image': 'Exit.png', 'background': [0, 0, 0], 'actions': {'on-press': ['Media:pauseplay'], 'on-release': []}}
     
     def getConfigLayout(self):
-        return Gtk.Button(label="Pause config")
+        if self.playerctlAvailable:
+            return
+        return Gtk.Label(label="Please install playerctl on your system")
     
     #custom functions
+    def runShellCommand(self, command):
+        # Run the shell command and capture the output
+        try:
+            output = subprocess.check_output(command, shell=True, text=True)
+        except subprocess.CalledProcessError as e:
+            return None        
+
+        # Return the output as a string
+        return output
     
 class Next(ActionBase):
     ACTION_NAME = "next"
